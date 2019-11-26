94d213d1d30943f56a91f6ce9b25e144aaa0d9ee
Domenico <domeniconicoli@gmail.com>
2019-11-26 21:39:09 +0100

    Initial Commit

---
+++ SaveCookiesExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveCookiesExample
+//
+//  Created by Domo on 26/11/2019.
+//  Copyright © 2019 Domo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
